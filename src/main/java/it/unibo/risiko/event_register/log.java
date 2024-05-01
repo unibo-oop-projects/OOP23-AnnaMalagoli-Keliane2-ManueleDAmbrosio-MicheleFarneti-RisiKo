542bc506a7fa63d8eea2d20b53704390ffa3a8f1
keliane2 <kelianetchoumkeu@gmail.com>
2024-05-02 00:47:06 +0200

    started log's interface creation

---
+++ src/main/java/it/unibo/risiko/event_register/log.java
@@ -0,0 +1,9 @@
+package it.unibo.risiko.event_register;
+
+/**
+ * This interface contains methods for keeping track of in-game events
+ * @author Keliane Tchoumkeu
+ */
+public interface log {
+    
+}
