929852b33658bf4bacaa3223c58f1a3576addd30
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2024-05-06 19:14:30 +0200

    Added Card class

--- src/main/java/it/unibo/risiko/Card.java
+++ src/main/java/it/unibo/risiko/Card.java
@@ -1,20 +1,41 @@
 package it.unibo.risiko;
-
+/**
+ * The class Card is used to implement the cards of the game. 
+ * They can show the player which continents they have at the beginning 
+ * of the match or theycan be played to have and addictional number of armies.
+ * 
+ * @author Anna Malagoli 
+ */
 public class Card {
     
-    private String typeName;
-    private String territoryName;
+    private final String typeName;
+    private final String territoryName;
 
-    public Card(String territoryName, String typeName){
+    /**
+     * Constructor is used to set the information a the card.
+     * 
+     * @param territoryName is the name of the territory in the card
+     * @param typeName is the name of the type of the card that can be of 
+     * three different types: Fante, Cavaliere, Cannone 
+     */
+    public Card(final String territoryName, final String typeName) {
         this.typeName = typeName;
         this.territoryName = territoryName;
     }
 
-    public String getTerritoryName(){
+    /**
+     * Method used to get the territory's name that is on the card.
+     * @return the name of the territory
+     */
+    public String getTerritoryName() {
         return this.territoryName;
     }
 
-    public String getTypeName(){
+    /**
+     * Method used to get the type of card.
+     * @return the name of the type
+     */
+    public String getTypeName() {
         return this.typeName;
     }
 }
