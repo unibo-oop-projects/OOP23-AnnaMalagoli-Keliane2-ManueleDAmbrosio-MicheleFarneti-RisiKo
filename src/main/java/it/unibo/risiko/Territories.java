f8a8684b42833e0e9776e5d86bb66781f41d25bd
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2024-05-06 13:02:10 +0200

    creation of class Territories used to set the territories used in the game from a text file

--- src/main/java/it/unibo/risiko/Territories.java
+++ src/main/java/it/unibo/risiko/Territories.java
@@ -3,52 +3,71 @@ package it.unibo.risiko;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Optional;
+/**
+ * That class is used by the controller at the start of the game to generate the list 
+ * of territories that are in the map. The information of the territories are 
+ * memorized in a file text that contains after the name every contry followed by a :
+ * the name of a territory and the list of the neighboring territories. 
+ */
 public class Territories {
 
-    private List<Territory> listTerritories = new ArrayList<>();
-
-    public Territories(String filePath){
-        File file = new File(filePath);
-        String absoluteFilePath = file.getAbsolutePath();
-        try(
-            final InputStream inputStream = new FileInputStream(absoluteFilePath);
-            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        ){
+    private final List<Territory> listTerritories = new ArrayList<>();
+    /**
+     * Contructor to set the list of territories by extracting informations 
+     * from a file text. 
+     * 
+     * @param filePath is the relative path of the text file
+     */
+    public Territories(final String filePath) {
+        final File file = new File(filePath);
+        final String absoluteFilePath = file.getAbsolutePath();
+        try {
+            InputStream inputStream = new FileInputStream(absoluteFilePath);
+            try {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             String stringRow;
             String continentName = " ";
             String nameTerritory;
             List<String> listNearTerritory;
 
-            while((stringRow = bufferedReader.readLine()) != null){
-                if(stringRow.contains(":")){
-                    continentName = stringRow.substring(0, stringRow.length() - 1);                
-                } else{
-                    String[] nations = stringRow.split(" ");
-                    nameTerritory = nations[0];
-                    listNearTerritory = Arrays.asList(nations);
-                    listNearTerritory.remove(0);
-                    Territory territory = new TerritoryImpl(nameTerritory, continentName, listNearTerritory);
+            stringRow = bufferedReader.readLine();
+            do {
+                if (stringRow.contains(":")) { 
+                    continentName = stringRow.substring(0, stringRow.length() - 1);
+                } else {
+                    final List<String> nations = Arrays.asList(stringRow.split(" "));
+                    nameTerritory = nations.get(0);
+                    listNearTerritory = new ArrayList<>(nations);
+                    listNearTerritory.remove(nameTerritory);
+                    final Territory territory = new TerritoryImpl(nameTerritory, continentName, listNearTerritory);
                     this.listTerritories.add(territory);
-
                 }
-            }
+                stringRow = bufferedReader.readLine();
+            } while (stringRow != null);
+            bufferedReader.close();
 
-
-
-        } catch(Exception e){
-            //gestione eccezzione
+        } catch (IOException e) {
+            listTerritories.clear();
+        }
+    } catch (FileNotFoundException e) {
+            listTerritories.clear();;
         }
     }
 
-    public List<Territory> getList(){
-        List<Territory> copyListTerritories = new ArrayList<>(this.listTerritories);
-        return copyListTerritories; 
+    /**
+     * Method used to get the list of all the territories extracted from the text file.
+     * @return the list of territories
+     */
+    public List<Territory> getList() {
+        return List.copyOf(listTerritories); 
     }
 }
