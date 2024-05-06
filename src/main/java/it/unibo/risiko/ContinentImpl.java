702776eef7494346afb9ca79476ee7067eac35cc
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2024-05-06 18:59:38 +0200

    Added Continent and ContinentImpl

---
+++ src/main/java/it/unibo/risiko/ContinentImpl.java
@@ -0,0 +1,48 @@
+package it.unibo.risiko;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Implementation of the interface Continent.
+ * @author Anna Malagoli 
+ */
+public class ContinentImpl implements Continent {
+
+    private final List<String> listTerritories = new ArrayList<>();
+    private final String name;
+
+    /**
+     * Constructor that is used to set the name of the continent created.
+     * @param name is the name of the continent
+     */
+    ContinentImpl(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Method that returns the continent's name.
+     * @return the name of the continent
+     */
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Method to get the list of territories of a continent.
+     * @return the list of territories
+     */
+    @Override
+    public List<String> getListTerritories() {
+        return List.copyOf(this.listTerritories);
+    }
+
+    /**
+     * Method used to add a territory in the list of territories of the continent.
+     * @param terr is the name of a territory
+     */
+    @Override
+    public void addTerritory(final String terr) {
+        listTerritories.add(terr);
+    } 
+}
