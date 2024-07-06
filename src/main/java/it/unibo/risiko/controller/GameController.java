a3b2e803c9120d189a8f9b657ace38ae84fb81a3
Michele farneti <michele.farneti@studio.unibo.it>
2024-07-06 15:43:54 +0200

    FIxed file separators for GameController

--- src/main/java/it/unibo/risiko/controller/GameController.java
+++ src/main/java/it/unibo/risiko/controller/GameController.java
@@ -1,5 +1,7 @@
 package it.unibo.risiko.controller;
 
+import java.io.File;
+
 import it.unibo.risiko.model.game.GameManager;
 import it.unibo.risiko.model.game.GameManagerImpl;
 import it.unibo.risiko.view.gameView.GameView;
@@ -13,9 +15,11 @@ import it.unibo.risiko.view.gameView.GameViewObserver;
 public class GameController implements GameViewObserver{
     private final GameManager model;
     private final GameView view;
-    private static final String saveGamesFilePath = "resources/savegames/savegames.json";
-    private static final String mapImagePath = "/maps/standardMap.png";
-    private static final String resourcesPackageString = "build/resources/main/it/unibo/risiko";
+    private static final String FILE_SEPARATOR = File.separator;
+    private static final String saveGamesFilePath = FILE_SEPARATOR + "resources" + FILE_SEPARATOR + "savegames" + FILE_SEPARATOR +"savegames.json";
+    private static final String mapImagePath = FILE_SEPARATOR + "maps" + FILE_SEPARATOR + "standardMap.png";
+    private static final String resourcesPackageString = "build" + FILE_SEPARATOR + "resources" + FILE_SEPARATOR + "main" + FILE_SEPARATOR + "it" + FILE_SEPARATOR + "unibo" + FILE_SEPARATOR + "risiko";
+    
 
     /**
     * Initialization of the Game controller with a GameManager as model field and a Java Swing view 
