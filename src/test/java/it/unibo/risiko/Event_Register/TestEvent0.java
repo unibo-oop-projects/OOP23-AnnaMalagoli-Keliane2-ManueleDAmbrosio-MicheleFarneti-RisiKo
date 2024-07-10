4a7daaef962ba10bec47b3737cda8b1524e60fc3
keliane2 <kelianetchoumkeu@gmail.com>
2024-07-10 21:45:05 +0200

    Fixed dependancy problem in tests

--- src/test/java/it/unibo/risiko/Event_Register/TestEvent0.java
+++ src/test/java/it/unibo/risiko/Event_Register/TestEvent0.java
@@ -12,34 +12,35 @@ import it.unibo.risiko.model.event.EventType;
 import it.unibo.risiko.model.map.Territory;
 import it.unibo.risiko.model.map.TerritoryImpl;
 import it.unibo.risiko.model.player.Player;
-import it.unibo.risiko.model.player.StdPlayer;
+import it.unibo.risiko.model.player.PlayerFactory;
+import it.unibo.risiko.model.player.SimplePlayerFactory;
 
 
 public class TestEvent0 {
-    // private Player eventLeader=new StdPlayer("black", 4);
-    // private Player eventLeaderAdversary=new StdPlayer("yellow", 0);
-    // private Territory attacker=new TerritoryImpl("Belgia", "Europe", new ArrayList<String>());
-    // private Territory defender=new TerritoryImpl("France", "Europe", new ArrayList<String>());
-    // private Event e=new EventImpl(EventType.ATTACK, attacker, defender, eventLeader, eventLeaderAdversary);
+    private PlayerFactory pf=new SimplePlayerFactory();
+    private Player eventLeader=pf.createStandardPlayer();
+    private Player eventLeaderAdversary=pf.createStandardPlayer();
+    private Territory attacker=new TerritoryImpl("Belgia", "Europe", new ArrayList<String>());
+    private Territory defender=new TerritoryImpl("France", "Europe", new ArrayList<String>());
+    private Event e=new EventImpl(EventType.ATTACK, attacker, defender, eventLeader, eventLeaderAdversary);
 
-    
-    // @Test
-    // void testGetEventType(){
-    //     assertEquals(EventType.ATTACK, EventType.ATTACK);
-    // }
+    @Test
+    void testGetEventType(){
+        assertEquals(EventType.ATTACK, EventType.ATTACK);
+    }
 
-    // @Test
-    // void testGetEventLeader(){
-    //     assertEquals(this.eventLeader, e.getEventLeader());
-    // }
+    @Test
+    void testGetEventLeader(){
+        assertEquals(this.eventLeader, e.getEventLeader());
+    }
 
-    // @Test
-    // void testGetAttackingTerritory(){
-    //     assertEquals(this.attacker, e.getAttackingTerritory());
-    // }
+    @Test
+    void testGetAttackingTerritory(){
+        assertEquals(this.attacker, e.getAttackingTerritory());
+    }
 
-    // @Test
-    // void testGetDefenderTerritory(){
-    //     assertEquals(this.defender, e.getDefenderTerritory());
-    // }
+    @Test
+    void testGetDefenderTerritory(){
+        assertEquals(this.defender, e.getDefenderTerritory());
+    }
 }
