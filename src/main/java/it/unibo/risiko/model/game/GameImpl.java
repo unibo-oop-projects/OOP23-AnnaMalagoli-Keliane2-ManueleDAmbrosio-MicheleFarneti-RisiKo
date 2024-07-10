35e316ea8b1bcf9b64874546886d08a494d8d807
Michele Farneti <michele.farneti@studio.unibo.it>
2024-07-10 14:26:45 +0200

    Fixed game loop

--- src/main/java/it/unibo/risiko/model/game/GameImpl.java
+++ src/main/java/it/unibo/risiko/model/game/GameImpl.java
@@ -1,4 +1,5 @@
 package it.unibo.risiko.model.game;
+
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,19 +32,19 @@ public class GameImpl implements Game {
     private final GameMap map;
 
     private int activePlayer = 0;
-    private int armiesPlaced = 0 ;
+    private int armiesPlaced = 0;
     private List<Player> players = new LinkedList<Player>();
     private GameStatus status = GameStatus.TERRITORY_OCCUPATION;
 
-    protected GameImpl(final GameMap map, final List<Player> players){
+    protected GameImpl(final GameMap map, final List<Player> players) {
         this.map = map;
         this.players.addAll(players);
     }
-   
+
     @Override
-    public void startGame(){
+    public void startGame() {
         Collections.shuffle(players);
-        players.forEach( p -> p.setArmiesToPlace(map.getStratingArmies(players.size())));
+        players.forEach(p -> p.setArmiesToPlace(map.getStratingArmies(players.size())));
         assignTerritories();
         assignTargets();
     }
@@ -52,36 +53,44 @@ public class GameImpl implements Game {
      * Assigns a target to every player
      */
     private void assignTargets() {
-        players.stream().forEach(p -> p.setTarget(generateRandomTarget(p)));  
+        players.stream().forEach(p -> p.setTarget(generateRandomTarget(p)));
     }
 
     /**
      * Gennerates a random target for a given player
-     * @param player The player who is getting the target 
-     * @return The random target 
+     * 
+     * @param player The player who is getting the target
+     * @return The random target
      */
-    private Target  generateRandomTarget(Player player){
-        switch(TargetType.randomTargetType()){
+    private Target generateRandomTarget(Player player) {
+        switch (TargetType.randomTargetType()) {
             case PLAYER:
-                return new DestroyPlayerTarget(player, players.get((players.indexOf(player) + randomNumberGenerator.nextInt(1,players.size()))%players.size()));
+                return new DestroyPlayerTarget(player, players.get(
+                        (players.indexOf(player) + randomNumberGenerator.nextInt(1, players.size())) % players.size()));
             case TERRITORY:
-                return new ConquerTerritoriesTarget(player,randomNumberGenerator.nextInt(Math.toIntExact(Math.round(map.getTerritories().size()*MIN_TERRITORIES_TO_CONQUER_PERCENTAGE)),Math.toIntExact(Math.round(map.getTerritories().size()*MAX_TERRITORIES_TO_CONQUER_PERCENTAGE))));
+                return new ConquerTerritoriesTarget(player, randomNumberGenerator.nextInt(
+                        Math.toIntExact(
+                                Math.round(map.getTerritories().size() * MIN_TERRITORIES_TO_CONQUER_PERCENTAGE)),
+                        Math.toIntExact(
+                                Math.round(map.getTerritories().size() * MAX_TERRITORIES_TO_CONQUER_PERCENTAGE))));
             case CONTINENT:
-                return new ConquerContinentTarget(player, map.getContinents().get(randomNumberGenerator.nextInt(map.getContinents().size())));
+                return new ConquerContinentTarget(player,
+                        map.getContinents().get(randomNumberGenerator.nextInt(map.getContinents().size())));
             default:
                 return new ConquerTerritoriesTarget(player, map.getTerritories().size());
         }
     }
 
     /**
-     * Private function used to split the map territories between the players in the game, it also
-     * places one army per territory for each player 
+     * Private function used to split the map territories between the players in the
+     * game, it also
+     * places one army per territory for each player
      */
     private void assignTerritories() {
         var territoriesToAssign = map.getTerritories();
         Collections.shuffle(territoriesToAssign);
 
-       for (Territory territory : territoriesToAssign) {
+        for (Territory territory : territoriesToAssign) {
             players.get(activePlayer).addTerritory(territory);
             territory.addArmies(1);
             players.get(activePlayer).decrementArmiesToPlace();
@@ -92,21 +101,19 @@ public class GameImpl implements Game {
     }
 
     @Override
-    public boolean nextTurn(){
-        if(skipTurnPossible()){
-            if(status == GameStatus.TERRITORY_OCCUPATION){
+    public boolean nextTurn() {
+        if (skipTurnPossible()) {
+            if (status == GameStatus.TERRITORY_OCCUPATION) {
                 armiesPlaced = 0;
-                if(getTotalArmiesLeftToPlace() == 0){
+                if (getTotalArmiesLeftToPlace() == 0) {
                     players.get(nextPlayerIfNotDefeated()).computeReinforcements();
-                    updateCurrentPlayer();
                     status = status.next();
-                } else{
-                    updateCurrentPlayer();
-                }
+                } 
+                updateCurrentPlayer();
                 return true;
-            } else if(status == GameStatus.ARMIES_PLACEMENT){
+            } else if (status == GameStatus.ARMIES_PLACEMENT) {
                 status = status.next();
-            } else if (status == GameStatus.READY_TO_ATTACK){
+            } else if (status == GameStatus.READY_TO_ATTACK) {
                 status = status.next();
                 players.get(nextPlayerIfNotDefeated()).computeReinforcements();
                 updateCurrentPlayer();
@@ -116,44 +123,44 @@ public class GameImpl implements Game {
         return false;
     }
 
-
     /**
      * @return False if the player is not allowed to skip his turn, true otherwise.
      */
-    private boolean skipTurnPossible(){
+    private boolean skipTurnPossible() {
         switch (status) {
             case TERRITORY_OCCUPATION:
-                return(armiesPlaced == PLACEABLE_ARMIES_PER_TURN || players.get(activePlayer).getArmiesToPlace() == 0);
+                return (armiesPlaced == PLACEABLE_ARMIES_PER_TURN || players.get(activePlayer).getArmiesToPlace() == 0);
             case ARMIES_PLACEMENT:
-                return players.get(activePlayer).getArmiesToPlace()==0;
+                return players.get(activePlayer).getArmiesToPlace() == 0;
             case READY_TO_ATTACK:
                 return true;
             default:
-                return players.get(activePlayer).getArmiesToPlace() == 0; 
+                return players.get(activePlayer).getArmiesToPlace() == 0;
         }
     }
 
     /**
-     * @return The totale amount of armies that are still left to be placed among all the players.
+     * @return The totale amount of armies that are still left to be placed among
+     *         all the players.
      */
-    private int getTotalArmiesLeftToPlace(){
+    private int getTotalArmiesLeftToPlace() {
         return players.stream().mapToInt(p -> p.getArmiesToPlace()).sum();
     }
 
     @Override
-    public boolean placeArmies(final Territory territory, final int nArmies){
-        if(players.get(activePlayer).getArmiesToPlace()>0){
-            if(status == GameStatus.TERRITORY_OCCUPATION){
-                if(armiesPlaced < 3){
-                    if(players.get(activePlayer).isOwnedTerritory(territory)){
+    public boolean placeArmies(final Territory territory, final int nArmies) {
+        if (players.get(activePlayer).getArmiesToPlace() > 0) {
+            if (status == GameStatus.TERRITORY_OCCUPATION) {
+                if (armiesPlaced < 3) {
+                    if (players.get(activePlayer).isOwnedTerritory(territory)) {
                         territory.addArmies(nArmies);
-                        armiesPlaced ++;
+                        armiesPlaced++;
                         players.get(activePlayer).decrementArmiesToPlace();
                         return true;
                     }
                 }
-            } else if ( status == GameStatus.ARMIES_PLACEMENT){
-                if(players.get(activePlayer).isOwnedTerritory(territory)){
+            } else if (status == GameStatus.ARMIES_PLACEMENT) {
+                if (players.get(activePlayer).isOwnedTerritory(territory)) {
                     territory.addArmies(nArmies);
                     players.get(activePlayer).decrementArmiesToPlace();
                     nextTurn();
@@ -175,12 +182,13 @@ public class GameImpl implements Game {
     }
 
     /**
-     * @return The index of the next active player, avoiding all of the elliminated players.
+     * @return The index of the next active player, avoiding all of the elliminated
+     *         players.
      */
-    private int nextPlayerIfNotDefeated(){
-        if(!(players.get((activePlayer+1)%players.size()).isDefeated())){
+    private int nextPlayerIfNotDefeated() {
+        if (!(players.get((activePlayer + 1) % players.size()).isDefeated())) {
             return nextPlayer();
-        }else{
+        } else {
             activePlayer = nextPlayer();
             return nextPlayer();
         }
@@ -191,38 +199,47 @@ public class GameImpl implements Game {
      * 
      * @return The index of the next player, independently if it is Defeated or not
      */
-    private int nextPlayer(){
-        return (activePlayer+1)%players.size();
+    private int nextPlayer() {
+        return (activePlayer + 1) % players.size();
     }
 
     /**
      * Sets as new activePlayer the next player in line, eventually handles the AI
      * moves if the next player is AI
      */
-    private void updateCurrentPlayer(){
+    private void updateCurrentPlayer() {
         activePlayer = nextPlayerIfNotDefeated();
         handleAIBehaviour();
     }
 
-    /** 
+    /**
      * 
      * Based on the game status, handles AI's behavior
      * 
      */
     private void handleAIBehaviour() {
-        if(getCurrentPlayer().isAI()){
-            var AIBehaviour = new AIBehaviourImpl(getCurrentPlayer());
+        if (getCurrentPlayer().isAI()) {
+            var aiBehaviour = new AIBehaviourImpl(getCurrentPlayer());
             switch (status) {
                 case TERRITORY_OCCUPATION:
-                case ARMIES_PLACEMENT:
-                        while(this.placeArmies(AIBehaviour.decidePositioning(),1));
-                        this.nextTurn();
+                    while (this.placeArmies(aiBehaviour.decidePositioning(), 1));
+                    this.nextTurn();
                     break;
-                case READY_TO_ATTACK:
-                    if(AIBehaviour.decideAttack(getTerritoriesList())){
-                        //TO DO
+                case ARMIES_PLACEMENT:
+                    while (this.placeArmies(aiBehaviour.decidePositioning(), 1));
+                    if (aiBehaviour.decideAttack(getTerritoriesList())) {
+                        AttackPhase attackPhase = new AttackPhaseImpl(
+                            getCurrentPlayer(), 
+                            aiBehaviour.getNextAttackingTerritory(getTerritoriesList()),
+                            aiBehaviour.decideAttackingArmies(),
+                            getOwner(aiBehaviour.getNextAttackedTerritory()),
+                            aiBehaviour.getNextAttackedTerritory());
+                        attackPhase.destroyArmies();
+                        attackPhase.conquerTerritory(aiBehaviour.getArmiesToMove());
                     }
                     this.nextTurn();
+                    break;
+                default:
                     break;
             }
         }
@@ -230,13 +247,17 @@ public class GameImpl implements Game {
 
     @Override
     public boolean gameOver() {
-        Optional<Player> winner = players.stream().filter(p -> p.getTarget().isAchieved()==true).findAny();
-        if(winner.isPresent()){
-            if(winner.get().equals(players.get(activePlayer))){
+        Optional<Player> winner = players.stream().filter(p -> p.getTarget().isAchieved() == true).findAny();
+        if (winner.isPresent()) {
+            if (winner.get().equals(players.get(activePlayer))) {
                 return true;
-            }
-            else{
-                players.get(activePlayer).setTarget(new ConquerTerritoriesTarget(players.get(activePlayer),randomNumberGenerator.nextInt(Math.toIntExact(Math.round(map.getTerritories().size()*MIN_TERRITORIES_TO_CONQUER_PERCENTAGE)),Math.toIntExact(Math.round(map.getTerritories().size()*MAX_TERRITORIES_TO_CONQUER_PERCENTAGE)))));
+            } else {
+                players.get(activePlayer).setTarget(new ConquerTerritoriesTarget(players.get(activePlayer),
+                        randomNumberGenerator.nextInt(
+                                Math.toIntExact(Math
+                                        .round(map.getTerritories().size() * MIN_TERRITORIES_TO_CONQUER_PERCENTAGE)),
+                                Math.toIntExact(Math
+                                        .round(map.getTerritories().size() * MAX_TERRITORIES_TO_CONQUER_PERCENTAGE)))));
             }
         }
         return false;
@@ -254,7 +275,8 @@ public class GameImpl implements Game {
 
     @Override
     public Player getOwner(Territory territory) {
-        return players.stream().filter(p -> p.getOwnedTerritories().stream().anyMatch( t-> t.equals(territory))).findFirst().get();
+        return players.stream().filter(p -> p.getOwnedTerritories().stream().anyMatch(t -> t.equals(territory)))
+                .findFirst().get();
     }
 
     @Override
@@ -264,14 +286,14 @@ public class GameImpl implements Game {
 
     @Override
     public void setAttacking() {
-        if(status == GameStatus.READY_TO_ATTACK){
+        if (status == GameStatus.READY_TO_ATTACK) {
             status = GameStatus.ATTACKING;
         }
     }
 
     @Override
     public void endAttack() {
-        if(status == GameStatus.ATTACKING){
+        if (status == GameStatus.ATTACKING) {
             status = GameStatus.READY_TO_ATTACK;
         }
     }
