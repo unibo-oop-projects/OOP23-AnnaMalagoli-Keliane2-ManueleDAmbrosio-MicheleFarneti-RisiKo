02cacaa1785167dd182adbfa048a0984a40b3111
Michele Farneti <michele.farneti@studio.unibo.it>
2024-07-21 20:32:15 +0200

    Fixes for territoryPlaceHolder s tests

--- src/test/java/it/unibo/risiko/view/gameview/components/mainpanel/TestTerritoryPlaceHolders.java
+++ src/test/java/it/unibo/risiko/view/gameview/components/mainpanel/TestTerritoryPlaceHolders.java
@@ -14,24 +14,25 @@ import it.unibo.risiko.model.map.TerritoryImpl;
 import it.unibo.risiko.view.gameview.components.Position;
 
 /**
- * A test for territory place hodlers
+ * A test for territory place hodlers.
  * 
  * @author Michele Farneti
  */
-public class TestTerritoryPlaceHolders {
+class TestTerritoryPlaceHolders {
     private static final String FILE_SEPARATOR = File.separator;
 
     @Test
-    void TestTerritoryPlaceHolder() {
+    void testTerritoryPlaceHolder() {
         final String coordinatesPath = "src" + FILE_SEPARATOR + "test" + FILE_SEPARATOR + "java" + FILE_SEPARATOR + "it"
                 + FILE_SEPARATOR + "unibo" + FILE_SEPARATOR + "risiko" + FILE_SEPARATOR + "view"
-                + FILE_SEPARATOR + "gameview" + FILE_SEPARATOR + "components" + FILE_SEPARATOR + "mainpanel" + FILE_SEPARATOR + "coordinates.txt";
+                + FILE_SEPARATOR + "gameview" + FILE_SEPARATOR + "components" + FILE_SEPARATOR + "mainpanel"
+                + FILE_SEPARATOR + "coordinates.txt";
         final TerritoryPlaceHolderFactory factory = new TerritoryPlaceHolderFactory(coordinatesPath);
         final Territory territory = new TerritoryImpl("China", "Asia", List.of());
         final Function<Position, Position> coordinatesFunction = new Function<>() {
 
             @Override
-            public Position apply(Position p) {
+            public Position apply(final Position p) {
                 return new Position(1, 1);
             }
 
